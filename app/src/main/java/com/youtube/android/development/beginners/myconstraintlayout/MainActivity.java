9d9375374fd51d55bf32e76d9786bc4783a8b4a3
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-06 11:12:41 -0400

    Initial commit

---
+++ app/src/main/java/com/youtube/android/development/beginners/myconstraintlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.youtube.android.development.beginners.myconstraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
